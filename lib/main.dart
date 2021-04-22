7bdcb1752ef0ded00d2545b2574fd306f535e08e
Marilyn-164 <marilynbrighty@gmail.com>
2021-04-22 13:10:20 +0530

    text widget modified

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'NextInGo App Demo'),
+      home: MyHomePage(title: 'NextInGo App Demo1'),
     );
   }
 }
