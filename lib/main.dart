c4578c1aab008863aaccee0543fb720577ce2297
Marilyn-164 <marilynbrighty@gmail.com>
2021-04-25 14:02:55 +0530

    partial login

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-       
-      },
+      routes: {},
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -37,12 +35,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     final loginButton = Material(
-      elevation: 5,
-      borderRadius: BorderRadius.circular(10),
+
+      elevation: 6,
+      borderRadius: BorderRadius.circular(40),
       color: Colors.blue,
       child: MaterialButton(
         onPressed: () {},
@@ -50,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Text(
           'Sign in',
           style: GoogleFonts.montserrat(
-            fontSize: 20,
+            fontSize: 25,
             fontWeight: FontWeight.w800,
             color: Colors.white,
           ),
@@ -59,35 +55,22 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
 
-
-
-
-
-
-
     final txtField1 = TextField(
-
+    
       textInputAction: TextInputAction.next,
       obscureText: false,
       style: GoogleFonts.montserrat(
         fontSize: 20.0,
       ),
       decoration: InputDecoration(
+        
         contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         hintText: 'Email',
-        border: OutlineInputBorder(),
+        border: OutlineInputBorder( borderRadius: BorderRadius.circular(40),),
       ),
-
-
-
     );
 
-
-
-
-
     final txtField2 = TextField(
-
       obscureText: true,
       style: GoogleFonts.montserrat(
         fontSize: 20.0,
@@ -95,28 +78,76 @@ class _MyHomePageState extends State<MyHomePage> {
       decoration: InputDecoration(
         contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         hintText: 'Password',
-        border: OutlineInputBorder(),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(40),),
       ),
     );
 
-
-final img = Image.asset('assets/1.png',);
-
+    final  img1 = Image.asset(
+      'assets/1.png',
+    );
+    final img2 = CircleAvatar(
+      backgroundImage: NetworkImage(
+          'https://images.unsplash.com/photo-1487243528516-7fa712e910f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80'),
+      radius: 100.0,
+      backgroundColor: Colors.transparent,
+    );
 
     return Scaffold(
-     
+      
       body: ListView(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-             
-                        img,
-                        txtField1,
-                        txtField2,
-                        loginButton,
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Column(
+mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-            ],
+                  children: [
+                    img1,
+                    SizedBox(height: 30.0,),
+                    txtField1,
+                    SizedBox(height: 10.0,),
+                    txtField2,
+                    SizedBox(height: 20.0,),
+                    Container(
+                      width: 200.0,
+                      child: loginButton),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          child: Text(
+                            'forgot password?',
+                            style: GoogleFonts.montserrat(
+                              fontSize: 15,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+               SizedBox(height: 50.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have account?',style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                        ),),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Sign up',style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                        ),),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
